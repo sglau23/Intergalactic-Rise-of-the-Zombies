9b7e9a0eee4e50c0970cca666fda4682ba864b06
Evan Doubek <doubek.evan@gmail.com>
2026-04-29 22:33:50 -0500

    Fix?

--- Final/Assets/Scripts/TickUpdates.cs
+++ Final/Assets/Scripts/TickUpdates.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class TickUpdates : MonoBehaviour
 {
+
+    [System.Serializable]
+    public struct CylinderData
+    {
+        public Transform cylinder;
+        public float heightMultiplier;
+        [HideInInspector] public float baseY;
+    }
+    public CylinderData[] cylinders = new CylinderData[13];
+
     float timer = 0f;
 
     void Start()
@@ -12,11 +22,16 @@ public class TickUpdates : MonoBehaviour
         }
         for (int i = 0; i < GlobalVariables.capsulePS.Length; i++)
         {
-            GlobalVariables.capsulePS[i] = 0;
+            GlobalVariables.capsulePS[i] = 1;
         }
         for (int i = 0; i < GlobalVariables.capsuleGens.Length; i++)
         {
             GlobalVariables.capsuleGens[i] = new GlobalVariables.CapsuleGen { count = 0, ps = 1, price = 100, upgradePrice = 1000 };
+        }
+        for (int i = 0; i < cylinders.Length; i++)
+        {
+            cylinders[i].baseY = cylinders[i].cylinder.position.y - cylinders[i].cylinder.localScale.y;
+            cylinders[i].heightMultiplier = 0.0025f; // Adjust this value to control how much the cylinder grows per capsule
         }
     }
 
@@ -30,6 +45,19 @@ public class TickUpdates : MonoBehaviour
             {
                 GlobalVariables.capsuleCount[i] += GlobalVariables.capsulePS[i];
             }
+        }
+
+        for (int i = 0; i < cylinders.Length; i++)
+        {
+            float newScaleY = Mathf.Max(0.01f, GlobalVariables.capsuleCount[i] * cylinders[i].heightMultiplier);
+
+            Vector3 scale = cylinders[i].cylinder.localScale;
+            scale.y = newScaleY;
+            cylinders[i].cylinder.localScale = scale;
+
+            Vector3 pos = cylinders[i].cylinder.position;
+            pos.y = cylinders[i].baseY + newScaleY;
+            cylinders[i].cylinder.position = pos;
         }
     }
 }
