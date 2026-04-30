33e148f9055aa02e371a0bdc0b0412b2065fba9c
Savannah Lau <slau24@illinois.edu>
2026-04-30 00:58:17 -0500

    scripts for timer + score board

--- Final/Assets/TeleportScript.cs
+++ Final/Assets/TeleportScript.cs
@@ -3,17 +3,36 @@ using UnityEngine;
 public class TeleportScript : MonoBehaviour
 {
     public Transform[] levelPoints;
-
+    public int hubIndex =6; // index for main area
+    private int currLevel = -1; 
     public void TeleportToLevel(int index)
 {
-    if (index >= 0 && index < levelPoints.Length)
+    if (index < 0 || index >= levelPoints.Length)
     {
-        Transform target = levelPoints[index];
+        return; 
+    }
+    Transform target = levelPoints[index];
 
         // Preserve headset height offset
-        Vector3 offset = transform.position - Camera.main.transform.position;
+    Vector3 offset = transform.position - Camera.main.transform.position;
 
-        transform.position = target.position + offset;
+    transform.position = target.position + offset;
+    if(index != hubIndex){
+        currLevel = index; 
+        if(TimeManager.Instance != null){
+            TimeManager.Instance.StartTimer(); 
+        }
+    }
+    else if (currLevel != -1){
+        if (TimeManager.Instance != null && ScoreManager.Instance != null)
+            {
+                float time = TimeManager.Instance.StopTimer();
+                ScoreManager.Instance.SaveTime(currLevel, time);
+
+                Debug.Log($"Level {currLevel} completed in {time:F2}s");
+            }
+
+            currLevel = -1;
     }
 }
 }
