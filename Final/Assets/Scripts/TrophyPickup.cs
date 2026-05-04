3de8e7a77771a07d8fa4007bb219e369906a5e44
lnava91 <lnava91@illinois.edu>
2026-05-04 12:22:06 -0500

    changed player height

---
+++ Final/Assets/Scripts/TrophyPickup.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TrophyPickup : MonoBehaviour
+{
+    public enum TrophyType
+    {
+        Bronze,
+        Silver,
+        Gold
+    }
+
+    public TrophyType trophyType;
+
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
+
+    void Awake()
+    {
+        grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrabbed);
+        }
+    }
+
+    private void OnGrabbed(SelectEnterEventArgs args)
+    {
+        if (trophyType == TrophyType.Bronze)
+        {
+            GlobalVariables.bronzeTropyGrabbed = true;
+        }
+        else if (trophyType == TrophyType.Silver)
+        {
+            GlobalVariables.silverTropyGrabbed = true;
+        }
+        else if (trophyType == TrophyType.Gold)
+        {
+            GlobalVariables.goldTropyGrabbed = true;
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    void OnDestroy()
+    {
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.RemoveListener(OnGrabbed);
+        }
+    }
+}
